6a87ef352b7b7400007a363c42ff64d2599f8b98
jhonataraibolt <jhonata.raibolt@residente.serratec.org>
2026-06-18 22:07:51 -0300

    correções

--- src/components/CardPublicacao/cardMentoria.tsx
+++ src/components/CardPublicacao/cardMentoria.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Mentoria } from '../../@types/mentoria';
-import {styles} from './styleCardMentorias';
+import {Styles} from './styleCardMentorias';
 
 interface CardMentoriaProps {
   dados: Mentoria;
@@ -10,13 +10,13 @@ interface CardMentoriaProps {
 
 export function CardMentoria({ dados, aoPressionar }: CardMentoriaProps) {
   return (
-    <TouchableOpacity style={styles.container} onPress={aoPressionar} activeOpacity={0.7}>
-      <Text style={styles.titulo}>{dados.titulo}</Text>
-      <Text style={styles.mentor}>Mentor(a): {dados.nomeMentor}</Text>
+    <TouchableOpacity style={Styles.container} onPress={aoPressionar} activeOpacity={0.7}>
+      <Text style={Styles.titulo}>{dados.titulo}</Text>
+      <Text style={Styles.mentor}>Mentor(a): {dados.nomeMentor}</Text>
       
-      <View style={styles.rodape}>
-        <Text style={styles.info}>{dados.data} às {dados.hora}</Text>
-        <Text style={styles.local} numberOfLines={1}>📍 {dados.local}</Text>
+      <View style={Styles.rodape}>
+        <Text style={Styles.info}>{dados.data} às {dados.hora}</Text>
+        <Text style={Styles.local} numberOfLines={1}>📍 {dados.local}</Text>
       </View>
     </TouchableOpacity>
   );
