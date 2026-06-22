6f58ef7816014cefcbed1ca9ed47fbdc0e23f0fd
Lucas Alves <lucasalvesdacruz0807@gmail.com>
2026-06-22 02:41:24 -0300

    subindo alterações

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import Toast from 'react-native-toast-message';
 
-import { MetasStackRoutes } from './src/routes/metasStackRoutes';
+import { MetasStackRoutes } from './src/routes/metas.routes';
 import { ProgressProvider } from './src/contexts/ProgressContext';
 
 export default function App() {
